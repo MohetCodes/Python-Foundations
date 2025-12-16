64b06eeaa8c6e04a27094a89047101a31fa313d7
MohetCodes <mohetkumar69409@gmail.com>
2025-12-16 19:59:42 +0500

    Add files via upload
    Add password validator v2 with loop and function

---
+++ Practice.py
@@ -0,0 +1,22 @@
+# def passes_check():
+#     password = "mohey123"
+#     passes = input("please enter password")
+#     while len(passes)<6 or passes != password:
+#         passes = input("please enter password\n incorrect pass try agan")
+#     return "access granted"
+# x=passes_check()
+# print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
